deec4078fb30dc70cdfa17b82931a07f47592af9
POOJASREEH <poojasreeh17@gmail.com>
2025-11-19 08:57:00 +0530

    Update memory.py

--- src/meta_agent/memory.py
+++ src/meta_agent/memory.py
@@ -1,1 +1,19 @@
+# src/meta_agent/memory.py
+"""
+Very small memory module used to keep session-level info.
+"""
+from typing import Dict, Any
+
+class InMemoryMemory:
+    def __init__(self):
+        self._store = {}
+
+    def write(self, key: str, value: Any):
+        self._store[key] = value
+
+    def read(self, key: str, default=None):
+        return self._store.get(key, default)
+
+    def dump(self):
+        return dict(self._store)
 
