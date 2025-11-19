31b7f24f27094825dd514faee4485c813779d108
POOJASREEH <poojasreeh17@gmail.com>
2025-11-19 09:00:38 +0530

    Update config.py

--- src/infra/config.py
+++ src/infra/config.py
@@ -1,1 +1,6 @@
+# src/infra/config.py
+import os
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+DEFAULT_MISSION_FILE = os.path.join(DATA_DIR, "missions.yml")
 
